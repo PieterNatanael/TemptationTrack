e7dbb093c7b3f7f33f552527cbddae39ef15d712
PieterNatanael <pieternatanael@icloud.com>
2023-11-01 15:33:03 +0800

    Initial Commit

---
+++ TemptationTrack/TemptationTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemptationTrackApp.swift
+//  TemptationTrack
+//
+//  Created by Pieter Yoshua Natanael on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TemptationTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
