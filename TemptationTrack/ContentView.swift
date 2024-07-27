0678094e6302fdce6fd8c6d2a89ee30b02571db0
PieterNatanael <pieternatanael@icloud.com>
2024-07-27 19:24:01 +0800

    Update README.md

--- TemptationTrack/ContentView.swift
+++ TemptationTrack/ContentView.swift
@@ -331,6 +331,7 @@ struct ShowExplainView: View {
                 }
                 
                 
+                
 
                Button("Close") {
                    // Perform confirmation action
